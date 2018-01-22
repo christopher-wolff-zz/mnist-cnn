cb60e0172b4ccbc9c3284bc2dd2f0f35d940199d
Christopher Wolff <christopher.wolff@duke.edu>
2018-01-21 22:52:37 -0500

    Add data visualization and update gitignore

--- __main__.py
+++ __main__.py
@@ -1,1 +1,18 @@
 """Trains a CNN on the MNIST dataset."""
+
+
+# import keras
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+# import numpy as np
+
+
+# import mnist dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+# visualize dataset
+img_num = 50
+plt.imshow(x_train[img_num], cmap='gray')
+plt.title(f'Label: {y_train[img_num]}')
+plt.show()
